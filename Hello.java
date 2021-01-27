0af5a797aaa0b1e41905ee9d46bf4bd78186c99f
Bernard-Baah <78100467+Bernard-Baah@users.noreply.github.com>
2021-01-27 14:21:46 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************************************************************
+* Hello World
+*************************************************************************
+* Bernard Baah
+* 01/27/2021
+* CMSC255/003
+************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Bernard Baah>");
+	System.out.println("<Hello World>");
+	System.out.println("<Lab 1>");
+	System.out.println("<01/27/2021>");
+	System.out.println("<CMSC255/003>");
+	System.out.println();
+	}
+}
